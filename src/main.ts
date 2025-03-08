a4f9f2bdbb98bfd954bacc28d2ca0cfa48cf825f
simon1uo <simon1uo@163.com>
2025-03-08 23:39:53 +0800

    feat: update unocss config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'virtual:uno.css'
+import 'uno.css'
+// import 'virtual:uno.css'
+// import 'virtual:unocss-devtools'
+import '@unocss/reset/normalize.css'
+import './main.css'
 
 const app = createApp(App)
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,40 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetWebFonts,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  shortcuts: [
+    { 'bg-base': 'bg-white dark:bg-black', 'color-base': 'text-black dark:text-white' },
+  ],
+  theme: {
+    colors: {
+      // ...
+    },
+  },
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        sans: 'Inter',
+        mono: 'DM Mono',
+        condensed: 'Roboto Condensed',
+        wisper: 'Bad Script',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })
